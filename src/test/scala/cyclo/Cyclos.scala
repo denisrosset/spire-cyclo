9d7098eaab159cba1c7460fb321f02bea2774909
Denis Rosset <physics@denisrosset.com>
2016-04-24 20:59:53 +0800

    More tests!

--- src/test/scala/cyclo/Cyclos.scala
+++ src/test/scala/cyclo/Cyclos.scala
@@ -11,14 +11,36 @@ object Cyclos {
     override def apply(a: Cyclo): Boolean = !a.isZero
   }
 
+  def genNonZeroCyclo: Gen[Cyclo] = Gen.oneOf(
+    for {
+      n <- Gen.choose(1, 10)
+      d <- Gen.choose(1, 10)
+    } yield Cyclo(Rational(n, d)),
+    for {
+      n <- Gen.choose(-10, -1)
+      d <- Gen.choose(1, 10)
+    } yield Cyclo(Rational(n, d)),
+    Gen.choose(1, 5).map(k => Cyclo.e(k))
+  )
+
+  def genSimpleCyclo: Gen[Cyclo] = Gen.oneOf(
+    for {
+      n <- Gen.choose(-10, 10)
+      d <- Gen.choose(1, 10)
+    } yield Cyclo(Rational(n, d)),
+    Gen.choose(1, 5).map(k => Cyclo.sinRev(Rational(1, k))),
+    Gen.choose(1, 5).map(k => Cyclo.e(k))
+  )
+
+
   implicit def arbCyclo: Arbitrary[Cyclo] = Arbitrary {
     Gen.oneOf(
-      for {
-        n <- Gen.choose(-10, 10)
-        d <- Gen.choose(1, 10)
-      } yield Cyclo(Rational(n, d)),
-      Gen.choose(1, 20).map(k => Cyclo.sinRev(k)),
-      Gen.choose(1, 20).map(k => Cyclo.e(k))
+      genSimpleCyclo,
+      genNonZeroCyclo.map(_.reciprocal),
+      for(x <- genSimpleCyclo; y <- genNonZeroCyclo) yield x / y,
+      for(x <- genSimpleCyclo; y <- genSimpleCyclo) yield x * y,
+      for(x <- genSimpleCyclo; y <- genSimpleCyclo) yield x + y,
+      for(x <- genSimpleCyclo; y <- genSimpleCyclo) yield x - y
     )
   }
 
